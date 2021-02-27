6ba503e83ab9f1e87776fd6c7cb97c4e72bfa0b3
danil <danilsubath@yandex.ru>
2021-02-27 17:31:50 +0700

    ddd

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a0(7%bsbqol6whq45i%wn-)x@rl-v8y4=&)!ro(w7u8d4($)hf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['daniilprogramist.pythonanywhere.com']
+ALLOWED_HOSTS = ['daniilprogramist.pythonanywhere.com','127.0.0.1','localhost' ]
 
 
 # Application definition
--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<p>alive</p>")
-
+def meme(request):
+    return HttpResponse('<img src="https://w7.pngwing.com/pngs/892/977/png-transparent-bicycle-wheels-cycling-minibike-motorcycle-gears-bicycle-motorcycle-transport.png">')
 # Create your views here.
